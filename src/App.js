100c85c7eb112b97480faa0dbc28fde5b2d105f7
Guy Vider <guy@guyvider.com>
2019-03-10 13:00:33 -0700

    added editable state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, {useState} from 'react';
-import Locale from './Locale'
+import Locale from './Locale';
 import Time from './Time';
 import Editable from './Editable';
 
 export default () => {
   const defaultLocale = 'en-US';
-  let [locale, setLocale] = useState(defaultLocale);
+  const [locale, setLocale] = useState(defaultLocale);
+  const [value, setValue] = useState('');
+  
   return (
     <div>
-      <Locale defaultLocale={defaultLocale} onChange={setLocale} />
-      &nbsp;
       <Time locale={locale}/>
       <br />
-      <Editable label="Edit content:" initialValue="Hello" onValueChanged={console.log}/>
+      <Locale defaultLocale={defaultLocale} onChange={setLocale} />
+      <br />
+      <Editable label="Edit content:" initialValue="What's your name?" onChange={setValue}/>
+      <br/>
+      {value ? `hello ${value}` : null}
     </div>
   );
 }
